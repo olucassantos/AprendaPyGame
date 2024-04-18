727a7857b2d88929234822423690cf3bb1479ad2
Lucas Santos <lucas.santos@sp.senai.br>
2024-04-18 19:14:22 -0300

    Atualizar criação e movimentação da bola no jogo

--- pinga.py
+++ pinga.py
@@ -3,6 +3,7 @@ import random
 
 # Cria uma nova bola
 def novaBola():
+    # Cria um dicionário com as informações da bola
     cores = {
         1: (255, 0, 0),
         2: (0, 255, 0),
@@ -16,11 +17,12 @@ def novaBola():
         10: (128, 0, 0),
     }
 
-    velocidade = random.randint(1, 10)
-    cor = cores[velocidade]
-    tamanho = random.randint(10, 30)
-    vidas = random.randint(1, 10)
+    velocidade = random.randint(1, 10) # Velocidade aleatória
+    cor = cores[velocidade] # Cor de acordo com a velocidade
+    tamanho = random.randint(10, 30) # Tamanho aleatório
+    vidas = random.randint(1, 10) # Vidas aleatórias
 
+    # Retorna o dicionário com as informações da bola para ser adicionado na lista
     return {
         "posicao": [random.randint(200, 600), random.randint(200, 400)],
         "velocidade": velocidade,
@@ -30,6 +32,7 @@ def novaBola():
         "direcao": pygame.Vector2(1, 1)
     }
 
+# Inicializa o Pygame
 pygame.init()
 # Configurações da tela
 tamanho = (800, 600)
@@ -48,7 +51,7 @@ raio = 50
 velocidade = 0
 cor_tela = (255, 255, 255)
 
-# Lista de bolas
+# Lista de bolas que vão ser desenhadas e movimentadas
 listaBolas = []
 
 # Evento para o tempo
@@ -70,10 +73,11 @@ while True:
             pygame.quit() # Fecha o Pygame
             exit() # Fecha o programa
 
+        # Verifica se o evento é de tecla pressionada
         if evento.type == pygame.KEYDOWN:
-            if evento.key == pygame.K_UP:
+            if evento.key == pygame.K_UP: # Se a tecla for a seta para cima
                 velocidade += 1
-            elif evento.key == pygame.K_DOWN:
+            elif evento.key == pygame.K_DOWN: # Se a tecla for a seta para baixo
                 velocidade -= 1
     #########################################
 
@@ -98,21 +102,21 @@ while True:
         if bola["posicao"][0] >= tamanho[0] - bola["tamanho"]:
             # Reposiciona a bola para não sair da tela
             bola["posicao"][0] = tamanho[0] - bola["tamanho"]
-            bola["direcao"].x = -1
+            bola["direcao"].x = -1 # Inverte a direção da bola
         elif bola["posicao"][0] <= bola["tamanho"]:
             # Reposiciona a bola para não sair da tela
             bola["posicao"][0] = bola["tamanho"]
-            bola["direcao"].x = 1
+            bola["direcao"].x = 1 # Inverte a direção da bola
 
         # Verifica se a bola bateu no eixo Y
         if bola["posicao"][1] >= tamanho[1] - bola["tamanho"]:
             # Reposiciona a bola para não sair da tela
             bola["posicao"][1] = tamanho[1] - bola["tamanho"]
-            bola["direcao"].y = -1
+            bola["direcao"].y = -1 # Inverte a direção da bola
         elif bola["posicao"][1] <= bola["tamanho"]:
             # Reposiciona a bola para não sair da tela
             bola["posicao"][1] = bola["tamanho"]
-            bola["direcao"].y = 1
+            bola["direcao"].y = 1 # Inverte a direção da bola
 
     #########################################
     # Atualiza a tela para exibir o que foi desenhado
